09ed50bc12197fe8af7ed8e9cfb28076f4f3df57
OmgD1e <Simplex.ru@mail.ru>
2022-07-13 21:32:10 +0300

    add game progression

---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+import getRandomNumber from '../rnd.js';
+
+import rules from '../index.js';
+
+const getProgression = (start, delta, length) => {
+  const result = [];
+  for (let i = 0; i < length; i += 1) {
+    result.push(start + delta * i);
+  }
+  return result;
+};
+
+const description = 'What number is missing in the progression?';
+const generateQA = () => {
+  const progression = getProgression(
+    getRandomNumber(1, 7),
+    getRandomNumber(3, 6),
+    getRandomNumber(6, 10),
+  );
+  const hiding = getRandomNumber(3, 5);
+  const answer = progression[hiding].toString();
+  progression[hiding] = '..';
+  const question = progression.join(' ');
+  return [question, answer];
+};
+
+const gameProgression = () => rules(description, generateQA);
+
+export default gameProgression;
