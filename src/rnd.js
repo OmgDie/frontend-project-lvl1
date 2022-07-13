4bf8cf4d475c31527192cd0743218605f9f2d2f0
OmgD1e <Simplex.ru@mail.ru>
2022-07-13 20:55:52 +0300

    fix rnd

--- src/rnd.js
+++ src/rnd.js
@@ -1,3 +1,3 @@
-const getRandomNumber = (max) => Math.floor(Math.random() * (max));
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 export default getRandomNumber;
