6dab9b7c71b59a6f9168f72d416b4bf1d886636b
OmgD1e <Simplex.ru@mail.ru>
2022-07-13 19:42:41 +0300

    add game calc

---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+import getRandomNumber from '../rnd.js';
+
+import rules from '../index.js';
+
+const calculating = (firstNum, op, secondNum) => {
+  let result;
+  switch (op) {
+    case '+':
+      result = firstNum + secondNum;
+      break;
+    case '-':
+      result = firstNum - secondNum;
+      break;
+    case '*':
+      result = firstNum * secondNum;
+      break;
+
+    default:
+  }
+  return result;
+};
+const description = 'What is the result of the expression?';
+const ops = ['+', '-', '*'];
+const generateQA = () => {
+  const firstNum = getRandomNumber(20);
+  const secondNum = getRandomNumber(10);
+  const op = ops[getRandomNumber(3)];
+  const question = `${firstNum} ${op} ${secondNum}`;
+  const answer = calculating(firstNum, op, secondNum).toString();
+  return [question, answer];
+};
+
+const gameCalc = () => rules(description, generateQA);
+
+export default gameCalc;
